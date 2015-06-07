1d57194559f98f7b4c129a31d574ccc339dd24d8
weratyr <weratyr@gmail.com>
2015-06-07 15:03:49 +0200

    finished ex2

--- src/main/scala/2_gettingStarted.scala
+++ src/main/scala/2_gettingStarted.scala
@@ -4,6 +4,10 @@ object Ex2 extends App {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
-  
+  def fib(n: Int): Int = {
+  	if (n == 0) 0
+  	else if (n == 1) 1
+  	else fib(n-1) + fib(n-2)
+  }
+
 }
